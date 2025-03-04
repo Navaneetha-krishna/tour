a20a22efc1b6d65bd490c08a87e93702306dab89
Navaneetha-krishna <113902124+Navaneetha-krishna@users.noreply.github.com>
2025-03-04 06:43:23 +0000

    Create webtour.java

---
+++ webtour.java
@@ -0,0 +1,12 @@
+public class TourWebsite {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our Tour Website!");
+        displayTours();
+    }
+
+    public static void displayTours() {
+        System.out.println("1. Beach Paradise - $500");
+        System.out.println("2. Mountain Adventure - $700");
+        System.out.println("3. City Explorer - $400");
+    }
+}
